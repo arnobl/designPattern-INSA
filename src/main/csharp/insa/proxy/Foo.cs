4f36f0d4ed94da58df84c376e2c79d18df1a0cb9
arnobl <blouin.arno@gmail.com>
2017-09-06 13:14:07 +0200

    csharp proxy

---
+++ src/main/csharp/insa/proxy/Foo.cs
@@ -0,0 +1,13 @@
+class Foo {
+    private Lazy < MyObject > bar = new Lazy < MyObject > (); 
+
+    public MyObject getBar() {
+        return bar.Value; 
+    }
+}
+
+public class MyObject:Object {
+    public MyObject() {
+        Console.WriteLine("Creating Object"); 
+    }
+}
