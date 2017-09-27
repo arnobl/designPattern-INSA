4cf678963043e0192053a61b842976ed5c858342
arnobl <blouin.arno@gmail.com>
2017-09-27 18:53:22 +0200

    active object (promise) in js

---
+++ src/main/js/promise/promiseExample.js
@@ -0,0 +1,34 @@
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+let studentID = 0;
+
+function jury() {
+    let currentStdID = ++studentID;
+
+    console.log('The jury is going to deliberate on ' + currentStdID);
+
+    // We make a new promise: we promise a numeric count of this promise, starting from 1 (after waiting 3s)
+    let p1 = new Promise(
+        // The resolver function is called with the ability to resolve or reject the promise
+        (resolve, reject) => {
+            console.log(`Deciding whether the student ${currentStdID} will be graduated. Complex heuristic.`);
+            const willpass = Math.random() >= 0.5;
+
+            if(willpass) {
+                setTimeout(() => resolve(`very good student ${currentStdID}`), Math.random() * 3000);
+            }else {
+                setTimeout(() => reject(`Did not work enough! ${currentStdID}`), Math.random() * 3000);
+            }
+        }
+    );
+
+    p1.then(sucessMsg => console.log(`${sucessMsg} graduated!`)).catch(failMsg => console.log(`Did not graduated :s: ${failMsg}`));
+}
+
+jury();
+jury();
+jury();
+jury();
+jury();
+
