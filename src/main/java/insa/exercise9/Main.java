32e8178eb1fe573f934b1fa89a64cf9abcc418c0
arnobl <blouin.arno@gmail.com>
2017-10-17 14:56:18 +0200

    exo9 visitor main

---
+++ src/main/java/insa/exercise9/Main.java
@@ -0,0 +1,7 @@
+package insa.exercise9;
+
+public class Main {
+	public static void main(final String[] args) {
+		final Tree tree = new Tree(new SubNode(new ValueNode(1), new ValueNode(2)), "foo");
+	}
+}
