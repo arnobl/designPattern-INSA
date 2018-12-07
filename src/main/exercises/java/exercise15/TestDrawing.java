4baedce1cd3c27873d7c494897794f86becbca35
arnobl <1052622+arnobl@users.noreply.github.com>
2018-12-07 10:27:51 +0100

    up

---
+++ src/main/exercises/java/exercise15/TestDrawing.java
@@ -0,0 +1,29 @@
+package exercise15;
+
+public class TestDrawing {
+	Canvas drawingArea;
+
+	void testRectangleCreated() {
+	}
+}
+
+
+class Robot {
+	static void moveTo() {
+	}
+
+	static void press() {
+	}
+
+	static void release() {
+	}
+}
+
+class Canvas {
+	void setEditingMode(final EditingMode mode) {
+	}
+}
+
+enum EditingMode {
+	RECT;
+}
