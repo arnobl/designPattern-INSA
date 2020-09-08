205ec76c78a38af8fa822b00c42368978e844970
arnobl <1052622+arnobl@users.noreply.github.com>
2020-09-08 11:29:24 +0200

    up

--- src/main/exercises/java/singleinstance/Foo.java
+++ src/main/exercises/java/singleinstance/Foo.java
@@ -1,7 +1,22 @@
 package singleinstance;
 
 public class Foo {
+	private int f;
+
+	public Foo() {
+		super();
+		f = 10;
+	}
+
 	public int getFooFoo() {
-		return RandomGenerator.INSTANCE.nextInt() * 20;
+		return RandomGenerator.INSTANCE.nextInt() * f;
+	}
+
+	public int getF() {
+		return f;
+	}
+
+	public void setF(final int f) {
+		this.f = f;
 	}
 }
