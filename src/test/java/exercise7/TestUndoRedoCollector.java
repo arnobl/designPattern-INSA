c522e81ea9fb6c8a81cc8a2b5741316f64078762
arnobl <1052622+arnobl@users.noreply.github.com>
2020-09-14 15:50:15 +0200

    up

---
+++ src/test/java/exercise7/TestUndoRedoCollector.java
@@ -0,0 +1,133 @@
+package exercise7;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestUndoRedoCollector {
+	UndoRedoCollector collector;
+	Undoable undoable;
+	Undoable undoable2;
+
+	@BeforeEach
+	void setUp() {
+		collector = new UndoRedoCollector();
+		undoable = Mockito.mock(Undoable.class);
+		undoable2 = Mockito.mock(Undoable.class);
+	}
+
+	@Test
+	void add() {
+		collector.add(undoable);
+		assertEquals(1, collector.getNbUndoables());
+		assertEquals(0, collector.getNbRedoables());
+	}
+
+	@Test
+	void addNotNull() {
+		collector.add(null);
+		assertEquals(0, collector.getNbUndoables());
+		assertEquals(0, collector.getNbRedoables());
+	}
+
+	@Test
+	void undo() {
+		collector.add(undoable);
+		collector.undo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable, Mockito.never()).redo();
+		assertEquals(0, collector.getNbUndoables());
+		assertEquals(1, collector.getNbRedoables());
+	}
+
+	@Test
+	void undoEmpty() {
+		collector.undo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable, Mockito.never()).redo();
+		assertEquals(0, collector.getNbUndoables());
+		assertEquals(0, collector.getNbRedoables());
+	}
+
+	@Test
+	void undoUndoSingleElement() {
+		collector.add(undoable);
+		collector.undo();
+		collector.undo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable, Mockito.never()).redo();
+		assertEquals(0, collector.getNbUndoables());
+		assertEquals(1, collector.getNbRedoables());
+	}
+
+	@Test
+	void undoUndoTwoElements() {
+		collector.add(undoable);
+		collector.add(undoable2);
+		collector.undo();
+		collector.undo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable2, Mockito.times(1)).undo();
+		assertEquals(0, collector.getNbUndoables());
+		assertEquals(2, collector.getNbRedoables());
+	}
+
+	@Test
+	void redo() {
+		collector.add(undoable);
+		collector.undo();
+		collector.redo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable, Mockito.times(1)).redo();
+		assertEquals(1, collector.getNbUndoables());
+		assertEquals(0, collector.getNbRedoables());
+	}
+
+	@Test
+	void redo2Times() {
+		collector.add(undoable);
+		collector.undo();
+		collector.redo();
+		collector.undo();
+		collector.redo();
+		Mockito.verify(undoable, Mockito.times(2)).undo();
+		Mockito.verify(undoable, Mockito.times(2)).redo();
+	}
+
+	@Test
+	void redoRedo() {
+		collector.add(undoable);
+		collector.undo();
+		collector.redo();
+		collector.redo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable, Mockito.times(1)).redo();
+		assertEquals(1, collector.getNbUndoables());
+		assertEquals(0, collector.getNbRedoables());
+	}
+
+	@Test
+	void undoundoredoredo() {
+		collector.add(undoable);
+		collector.add(undoable2);
+		collector.undo();
+		collector.undo();
+		collector.redo();
+		collector.redo();
+		Mockito.verify(undoable, Mockito.times(1)).undo();
+		Mockito.verify(undoable, Mockito.times(1)).redo();
+		Mockito.verify(undoable2, Mockito.times(1)).undo();
+		Mockito.verify(undoable2, Mockito.times(1)).redo();
+	}
+
+	@Test
+	void cleanOnAdd() {
+		collector.add(undoable);
+		collector.undo();
+		collector.add(undoable2);
+		assertEquals(1, collector.getNbUndoables());
+		assertEquals(0, collector.getNbRedoables());
+	}
+}
