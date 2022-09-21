a1c4325d9d7b4ad9162eeb4b3a0c3d276494a702
arnobl <1052622+arnobl@users.noreply.github.com>
2022-09-21 17:00:06 +0200

    up

--- src/main/java/abstractFactory/api/ShapeFactory.java
+++ src/main/java/abstractFactory/api/ShapeFactory.java
@@ -1,6 +1,6 @@
 package abstractFactory.api;
 
-// Contrary the the Factory pattern, the base factory of the pattern Abstract Factory is... abstract (more specifically is an interface)
+// Contrary the Factory pattern, the base factory of the pattern Abstract Factory is... abstract (more specifically is an interface)
 // This abstract factory defines the services the concrete factories must implement.
 public interface ShapeFactory {
 	Polygon createPolygon();
