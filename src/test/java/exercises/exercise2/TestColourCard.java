262f226443e13af66af01ccdb26e3668f1e6bac5
arnobl <1052622+arnobl@users.noreply.github.com>
2023-09-14 08:19:19 +0200

    test colourcard

---
+++ src/test/java/exercises/exercise2/TestColourCard.java
@@ -0,0 +1,26 @@
+package exercises.exercise2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class TestColourCard {
+    @Test
+    void getNullReturnsEmpty() {
+        assertTrue(ColourCard.get(null).isEmpty());
+    }
+
+    @Test
+    void getBadStringReturnsEmpty() {
+        assertTrue(ColourCard.get("yolo").isEmpty());
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void getCorrectStringReturnsEmpty(final ColourCard cc) {
+        assertEquals(cc, ColourCard.get(cc.name()).orElseThrow());
+    }
+}
