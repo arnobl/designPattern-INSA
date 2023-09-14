89241ce3b52209c92aef90a4998f6bbc879eeb19
arnobl <1052622+arnobl@users.noreply.github.com>
2023-09-14 11:15:29 +0200

    up

--- src/test/java/exercises/exercise2/TestColourCard.java
+++ src/test/java/exercises/exercise2/TestColourCard.java
@@ -8,19 +8,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 public class TestColourCard {
-    @Test
-    void getNullReturnsEmpty() {
-        assertTrue(ColourCard.get(null).isEmpty());
-    }
+    // @Test
+    // void getNullReturnsEmpty() {
+    //     assertTrue(ColourCard.get(null).isEmpty());
+    // }
 
-    @Test
-    void getBadStringReturnsEmpty() {
-        assertTrue(ColourCard.get("yolo").isEmpty());
-    }
+    // @Test
+    // void getBadStringReturnsEmpty() {
+    //     assertTrue(ColourCard.get("yolo").isEmpty());
+    // }
 
-    @ParameterizedTest
-    @EnumSource
-    void getCorrectStringReturnsEmpty(final ColourCard cc) {
-        assertEquals(cc, ColourCard.get(cc.name()).orElseThrow());
-    }
+    // @ParameterizedTest
+    // @EnumSource
+    // void getCorrectStringReturnsEmpty(final ColourCard cc) {
+    //     assertEquals(cc, ColourCard.get(cc.name()).orElseThrow());
+    // }
 }
